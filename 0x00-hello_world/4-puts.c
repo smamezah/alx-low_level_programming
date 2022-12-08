efa1b4e58597f1832725bfc45efb2db2191c3800
Samuel Amezah <samuelamezah72@gmail.com>
2022-12-08 15:29:34 -0800

    added a c program that prints an array of text using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main -This prints an array of texts with puts
+ * Return: This return 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
