f15bbde9c4c44319a858e7360f91479d0b47d6ba
Samuel Amezah <samuelamezah72@gmail.com>
2022-12-08 15:36:42 -0800

    added a c program that prints an array of text using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - prints exactly an array of text with the printf function
+ * Return: This returns 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
