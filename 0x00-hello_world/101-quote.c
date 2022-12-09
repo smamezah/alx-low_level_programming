1cd5ff213cf8cf44c990a29ee8113eef767b0f5b
Samuel Amezah <samuelamezah72@gmail.com>
2022-12-08 16:20:40 -0800

    added a c program that prints output to the standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - This prints an array of texts with the fprintf and stderr
+ * Return: This returns 0
+ */
+int main(void)
+{
+	fprintf(stderr,"and that piece of art is useful" - Dora Korpar, 2015-10-19");
+	return (0);
+}
