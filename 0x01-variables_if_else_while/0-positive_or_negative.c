78ccc3c0fb877c2b533e93824209689f2768ef01
Samuel Amezah <samuelamezah72@gmail.com>
2022-12-09 15:14:48 -0800

    c program on if else was added

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,7 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
+
 /**
  * main - this code generates a random number and determine whether the number is positive or negative
  * Return: return 0
@@ -12,9 +14,9 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n < 0){
-		printf("%d is negative", n);
+		printf("%d is negative\n", n);
 	}
 	else {
-		printf("%d is positive", n);
+		printf("%d is positive\n", n);
 	return (0);
 }
