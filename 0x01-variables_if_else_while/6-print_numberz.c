b8a64fa2caad98bb28fec98eeedfb80330f0b978
Samuel Amezah <samuelamezah72@gmail.com>
2022-12-09 18:43:23 -0800

    a c program that prints numbers with only putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - write the decimal digits
+ * Returns: return 0
+ */
+
+int main(void)
+{
+	int i = 0;
+	while (i < 10)
+	{
+		putchar(48 + i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
+
