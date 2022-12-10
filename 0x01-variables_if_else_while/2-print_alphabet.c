dd2d044c18237143c53c85c99282d7a7c07489af
Samuel Amezah <samuelamezah72@gmail.com>
2022-12-09 16:36:58 -0800

    a c program that uses a for loop

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - this code prints the alphabets in lowercasse
+ * Return: this code returns 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+		putchar(\n);
+	}
+
+	return (0);
+}
