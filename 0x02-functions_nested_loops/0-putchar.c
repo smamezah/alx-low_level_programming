3e52cbdcc1863ffb973259bbd49c9afe8a0abe52
Samuel Amezah <samuelamezah72@gmail.com>
2022-12-13 15:26:28 -0800

    added a c program that prints putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this prints putchar
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This directory contain projects on nested loops and c functions
