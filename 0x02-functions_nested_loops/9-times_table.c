3dc0a0691ccdb5c5b854c5ab59579cbcebc4913f
Samuel Amezah <samuelamezah72@gmail.com>
2022-12-14 10:17:20 -0800

    added a c function that prints the 9 time table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 timetable
+ */
+
+void times_table(void)
+{
+	int i, j, x;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			for (x = 0; x <= 9, x++)
+			{
+				_putchar((j * x) + '0');
+				if (x != 9)
+				{
+					putchar(',');
+					putchar(' ');
+					putchar(' ');
+				}
+				else
+					break;
+			}
+
+		}
+		_putchar('\n');
+	}
+}
