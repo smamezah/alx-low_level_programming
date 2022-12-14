84b7036618c8ac4cf2c437552ab16188c09b217e
Samuel Amezah <samuelamezah72@gmail.com>
2022-12-14 15:22:37 -0800

    This function prints the 9x table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,23 +8,24 @@ void times_table(void)
 {
 	int num, multy, prod;
 
-	for (num = 0; num <= 10; num++)
+	for (num = 0; num < 10; num++)
 	{
-		_putchar(num + '0');
+		_putchar('0');
 
 		for (multy = 0; multy < 10; multy++)
 		{
 			prod = num * multy;
 			_putchar(',');
 			_putchar(' ');
-			_putchar(' ');
 
 			if (prod > 9)
 			{
+				
 				_putchar((prod / 10) + '0');
 				_putchar((prod % 10) + '0');
 			}
 			else
+				_putchar(' ');
 				_putchar(prod + '0');
 		}
 		_putchar('\n');
