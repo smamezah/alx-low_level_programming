dbc476d4b5b964e07da59aeb9f85994f86936f6e
Samuel Amezah <samuelamezah72@gmail.com>
2022-12-14 15:36:15 -0800

    a function that adds two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - print the sum of two integers
+ * @i : data type is int
+ *
+ * @j : data type is also int
+ */
+
+int add(int i, int j)
+{
+	int ret;
+
+	ret = i + j;
+
+	return (ret);
+}
