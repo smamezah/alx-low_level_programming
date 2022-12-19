e1d94cef0777012105df9feb6c68f591601612ce
Samuel Amezah <samuelamezah72@gmail.com>
2022-12-19 01:46:08 -0800

    print line function

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,13 +7,13 @@
 
 void print_line(int n)
 {
-	if (num <= 0)
+	if (n <= 0)
 		_putchar('\n');
 	else
 	{
 		int i;
 
-		for (i = 1; i <= num; i++)
+		for (i = 1; i <= n; i++)
 			_putchar('_');
 		_putchar('\n');
 	}
