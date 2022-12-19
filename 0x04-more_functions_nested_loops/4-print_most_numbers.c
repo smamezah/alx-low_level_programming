0736018ce28bbfe170595d57f51d621dfd026233
Samuel Amezah <samuelamezah72@gmail.com>
2022-12-19 00:45:24 -0800

    print most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints most digits
+ */
+
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i == 2 || i == 4)
+			continue;
+		else
+			_putchar(i + '0');
+	}
+	_putchar('\n');
+}
