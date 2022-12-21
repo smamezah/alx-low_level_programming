1e04646c3d957c85588ba5b43914d17118dca298
Samuel Amezah <samuelamezah72@gmail.com>
2022-12-20 16:18:40 -0800

    reverse a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: string
+ */
+
+void rev_string(char *s)
+{
+	char rev = s[0];
+	int i = 0, j;
+
+	while (s[i] != '\0')
+		i++;
+	for (j = 0; j < i; j++)
+	{
+		i--;
+		rev = s[j];
+		s[j] = s[i];
+		s[i] = rev;
+	}
+}
