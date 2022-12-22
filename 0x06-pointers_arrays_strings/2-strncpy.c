5a29fb911e90dea956ac5980c178dfc8828c0caa
Samuel Amezah <samuelamezah72@gmail.com>
2022-12-22 15:32:26 -0800

    2-strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strncpy - copy one string of up to n charcters into another
+ * @dest: destination
+ * @src: source
+ * @n: number of characters
+ * Return: return dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (i < n && src[i] = '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
