53e83ecf3b41f4fa3c296450a00c34f266a2f97c
Samuel Amezah <samuelamezah72@gmail.com>
2022-12-22 18:03:55 -0800

    6-cap_string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words in a string
+ * @n: string
+ * Return: returns n
+ */
+
+char *cap_string(char *n)
+{
+	int i = 0;
+
+	while (n[i] != '\0')
+	{
+		while (n[i] >= 'a' && n[i] <= 'z')
+		{
+			if (n[i - 1] == ' ' ||
+				n[i - 1] == '\t' ||
+				n[i - 1] == '\n' ||
+				n[i - 1] == ',' ||
+				n[i - 1] == ';' ||
+				n[i - 1] == '.' ||
+				n[i - 1] == '!' ||
+				n[i - 1] == '?' ||
+				n[i - 1] == '"' ||
+				n[i - 1] == '(' ||
+				n[i - 1] == ')' ||
+				n[i - 1] == '{' ||
+				n[i - 1] == '}' ||)
+			{
+				n[i] = n[i] - 32;
+			}
+		}
+		i++;
+	}
+	return (n);
+}
