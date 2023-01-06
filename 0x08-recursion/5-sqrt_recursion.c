fe399d2a6578e9d61a07236a2a991cd9d799c057
smamezah <samuelamezah72@gmail.com>
2023-01-06 04:40:27 +0000

    sqrt recursion function added

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,9 +1,37 @@
 #include "main.h"
 
 /**
+ * helperfunction - checks whether sqrt of a number exists
+ * @num: number
+ * @xroot: possible root
+ * Return: return square of number
+ * on error returns -1.
+ */
+int helperfunction(int num, int xroot)
+{
+	if ((xroot * xroot) == num)
+	{
+		return (xroot);
+	}
+	else
+	{
+		if ((xroot * xroot) > num)
+			return (-1);
+		else
+			return (helperfunction(num, xroot + 1));
+	}
+}
+
+/**
  * _sqrt_recursion - returns the natural square root
  * @n: number
  * Return: the square root of n
  */
 
-int 
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	else
+		return (helperfunction(n, 0));
+}
