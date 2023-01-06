7dfa710df211e7306687b984c63a5ba5895ed06f
smamezah <samuelamezah72@gmail.com>
2023-01-06 03:15:01 +0000

    debugged code

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,14 +4,13 @@
  * _puts_recursion - prints a string
  * @s: string to be printed
  */
-
 void _puts_recursion(char *s)
 {
-	if (*s)
+	if (*s == '\0')
 	{
-		_putchar(*s);
-		_putchar(s + 1);
-	}
-	else
 		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 
 void _puts_recursion(char *s);
-int _putchar(char c)
+int _putchar(char c);
 
 #endif
 
