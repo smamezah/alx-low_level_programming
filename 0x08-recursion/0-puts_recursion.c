86bd5e20de467c9295e8fb8051bca0df734e9e69
smamezah <samuelamezah72@gmail.com>
2023-01-06 01:31:51 +0000

    puts recursion function added

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string
+ * @s: string to be printed
+ */
+
+void _puts_recursion(char *s)
+{
+	if (s == '\0')
+		return (void);
+	_putchar(s[0]) + _puts_recursion(s + 1);
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void _puts_recursion(char *s);
+
+#endif
+
+
+
+
+
+
+
